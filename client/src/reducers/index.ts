0c32148511f237ad0cf59ca780bc4a1b5750a2ec
Suhas Shetty <shetty.suh@northeastern.edu>
2024-11-30 15:59:35 -0500

    bug fixes

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,6 +1,14 @@
 import { combineReducers } from "redux";
-import posts from './posts';
-import auth from './auth';
-export default combineReducers({
-    posts, auth 
+import posts from "./posts";
+import auth from "./auth";
+
+// Combine reducers
+const rootReducer = combineReducers({
+  posts,
+  auth,
 });
+
+// Define the RootState type for use in the application
+export type RootState = ReturnType<typeof rootReducer>;
+
+export default rootReducer;
